b99645974b5fdced25363d8f2a982d92cef58640
shihua-guo <shihua-guo@hotmail.com>
2019-04-30 14:04:15 +0800

    add views

---
+++ src/views/bar/index.js
@@ -0,0 +1,12 @@
+import router from '../../router';
+import template from './index.html';
+import './style.css';
+export default class {
+  mount(container) {
+    document.title = 'bar';
+    container.innerHTML = template;
+    container.querySelector('./bar__gofoo').addEventLIstener('click',()=>{
+      router.go('/foo');
+    });
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,4 @@
+const {resolve} = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const history = require('connect-history-fallback');
+const convert = require('koa-connect');
